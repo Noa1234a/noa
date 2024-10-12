e6302484cc364b43ef9fd54bd4f61b4cbf4c3555
Noa1234a <magalhaesnoa036@gmail.com>
2024-10-12 12:43:08 +0100

    Add files via upload

---
+++ Aula 1.py
@@ -0,0 +1,45 @@
+print("ola")
+#se eu  fizer uma funçao print, a consola vai  escrever aquilo que pus nos parantesis
+--------------COMENTARIOS--------------
+#O  comentario e uma parte o coigo que nao vai ser lida pelo programa, ou seja:
+#podemos escrever anotaçoes no codigo sem que elas perturbem o seu funcionamento
+#evido a sintaxe do phytin
+
+#Phytom = linguagem dde programaçao
+
+#linguagem = gramatica --> SINTAXE, ou seja, como a linguagem funciona
+
+palavra= "hamburger" # MAIS DOIS TIPOS DE DADOS: VARIÁVEL (que armazena um dado); e STRING (texto)
+# quando temos uma STRING podemos "copiar e colar" o que está dentro de aspas
+
+print("hamburger")
+print(palavra) #podemos usar as variáveis (texto branco) em qualquer parte do código
+
+#-------------------------------VARIÁVEIS---------------------------------------
+
+# uma variável contém um valor que pode mudar, como se fosse uma caixa em que podemos pôr várias coisas (e alterá-las)
+#REGRAS:
+# NÃO PODEM começar com números!
+
+print(5) #número fica AZUL
+# podemos fazer contas com NÚMEROS
+# se uma VARIÁVEL começar com um NÚMERO, o código vai AUTOMATICAMENTE pensar que estamos a tratar de um NÚMERO:
+
+porcento=5 #NO ENTANTO, podemos definir uma VARIÁVEL com apenas um número
+# isto
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
